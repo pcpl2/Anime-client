e7ed6126b8674f01ee9fc76b6554f750f6b85647
pcpl2 <patryk@bimgam.es>
2017-10-21 22:59:14 +0200

    Add autoupdater 1

--- js/app/app.js
+++ js/app/app.js
@@ -1,8 +1,42 @@
 var m = require("mithril");
 
+const AutoUpdater = require("nw-autoupdater"),
+    updater = new AutoUpdater(require("./package.json"), {
+        strategy: "ScriptSwap"
+    });
+
 m.route(document.getElementById("application"), "", {
     "/service/:sid/list": AnimeList,
     "/service/:sid/anime/:aid/list": EpisodeList,
     "/service/:sid/anime/:aid/episode/:eid": EpisodePlay,
     "": SelectService
-})
+});
+
+async function update() {
+    try {
+        // Download/unpack update if any available
+        const rManifest = await updater.readRemoteManifest();
+        const needsUpdate = await updater.checkNewVersion(rManifest);
+        if (!needsUpdate) {
+            return;
+        }
+        if (!confirm("New release is available. Do you want to upgrade?")) {
+            return;
+        }
+        // Subscribe for progress events
+        updater.on("download", (downloadSize, totalSize) => {
+            console.log("download progress", Math.floor(downloadSize / totalSize * 100), "%");
+        });
+        updater.on("install", (installFiles, totalFiles) => {
+            console.log("install progress", Math.floor(installFiles / totalFiles * 100), "%");
+        });
+        const updateFile = await updater.download(rManifest);
+        await updater.unpack(updateFile);
+        alert(`The application will automatically restart to finish installing the update`);
+        await updater.restartToSwap();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+update();
