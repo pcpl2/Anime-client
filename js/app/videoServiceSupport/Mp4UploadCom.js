9e6957be3c18b10515e3ea5edd4143bac56dca2d
pcpl2 <patryk@bimgam.es>
2017-12-04 22:40:15 +0100

    Fixed support for mp4upload.com

--- js/app/videoServiceSupport/Mp4UploadCom.js
+++ js/app/videoServiceSupport/Mp4UploadCom.js
@@ -6,7 +6,10 @@ this.Mp4UploadCom = {
     },
 
     getVideoUrl: function (url, returnFunction) {
-        const regexDataDecoder = /\,\'\|.+(\'\.split\(\'\|\'\))/igm;
+        var self = this;
+        const regexDataDecoder1 = /return p}(.+)\)/igm;
+        const regexDataDecoder2 = /\'(.+)\'\,/igm;
+        const regexDataDecoder3 = /src:"(.+)+"/igm;
         const regexValidateUrl = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/igm
         //check domain
         if (getDomainName(url) != this.domain) {
@@ -24,14 +27,22 @@ this.Mp4UploadCom = {
         }).then(function (res) {
             let videoUrl = $(parseHtml(res)).find("script").map(function() {
                 if(this.innerHTML.search("eval") != -1) {
-    
-                    let tmpData1 = regexDataDecoder.exec(this.innerHTML)[0].slice(2);
-                    let tmpData2 = tmpData1.slice(0, tmpData1.length - 12)
-                    let tmpData3 = tmpData2.split("|");
-                    console.log(tmpData3);
-    
-                    let url = tmpData3[4] + "://" + tmpData3[7] + "." + tmpData3[3] + "." + tmpData3[2] + ":" + tmpData3[20] + "/d/" + tmpData3[19] + "/" + tmpData3[9] + "." + tmpData3[6];
-                    console.log(url);
+
+                    let tmp = regexDataDecoder1.exec(this.innerHTML)[1].slice(1);
+                    let tmp1 = tmp.slice(0, tmp.length - 12);
+
+                    let arg1 = regexDataDecoder2.exec(tmp1)[1];
+                    
+                    let tmp2 = tmp1.slice(arg1.length + 3).split(",");
+
+                    let arg2 = tmp2[0];
+                    let arg3 = tmp2[1];
+                    let arg4 = tmp2[2].slice(1, tmp2[2].length - 1).split("|");
+
+                    let decodedString = self.decodeString(arg1, arg2, arg3, arg4); 
+
+                    let url = regexDataDecoder3.exec(decodedString)[1];
+
                     return url;
                 }
             }).get()[0];
@@ -44,5 +55,12 @@ this.Mp4UploadCom = {
         }).catch(function(e) {
             returnFunction("", VideoDecoderErrorCodes.VIDEO_NOT_FOUND);
         })
+    },
+
+    decodeString(p, a, c, k) {
+        while (c--)
+            if (k[c])
+                p = p.replace(new RegExp('\\b' + c.toString(a) + '\\b', 'g'), k[c]);
+        return p
     }
 }
