4c1b0d847a7760a063b38b88096f0f00236caf81
pcpl2 <patryk@bimgam.es>
2018-01-06 02:50:35 +0100

    Added show status of update application.

--- js/app/app.js
+++ js/app/app.js
@@ -25,17 +25,38 @@ async function update() {
             return;
         }
 
+        const progressDownload = $('.progress-bar-update-download');
+        progressDownload.text("0%");
+        const progressInstall = $('.progress-bar-update-install');
+        progressInstall.text("0%");
+
         updater.on("download", (downloadSize, totalSize) => {
-            console.log("download progress", Math.floor(downloadSize / totalSize * 100), "%");
+            let precent = Math.floor(downloadSize / totalSize * 100);
+            progressDownload.css('width', precent + '%').attr('aria-valuenow', precent);
+            progressDownload.text(precent + "%");
+            console.log("download progress", precent, "%");
         });
+
         updater.on("install", (installFiles, totalFiles) => {
-            console.log("install progress", Math.floor(installFiles / totalFiles * 100), "%");
+            let precent = Math.floor(downloadSize / totalSize * 100);
+            progressInstall.css('width', precent + '%').attr('aria-valuenow', precent);
+            progressInstall.text(precent + "%");
+            console.log("install progress", precent, "%");
         });
+
+        let downloadBox = $('#download-update');
+        let installBox = $('#install-update');
+
+        downloadBox.css("display", "block");
         const updateFile = await updater.download(rManifest);
+        downloadBox.css("display", "none");
+        installBox.css("display", "block");
         await updater.unpack(updateFile);
+        installBox.css("display", "none");
         alert(`The application will automatically restart to finish installing the update`);
         await updater.restartToSwap();
     } catch (e) {
+        alert('An error occurred while downloading / installing the update. The update has been canceled, another attempt will be made the next time the application is launched')
         console.error(e);
     }
 }
--- js/app/views/layout.js
+++ js/app/views/layout.js
@@ -1,16 +1,7 @@
-Navbar = {
-    view: function () {
-        return m("nav", { "class": "navbar navbar-dark bg-dark justify-content-between" }, [
-            m("strong", { "class": "navbar-brand" }, "Anime Client")
-        ]);
-    }
-};
-
 layout = function (breadcrumb, header, content) {
     return m("div", [
-        m(Navbar),
+        m("nav", { "class": "breadcrumb" }, [breadcrumb]),
         m("div", { "class": "container" }, [
-            m("nav", {"class": "breadcrumb"}, [breadcrumb]),
             m("div", { "class": "card fancy", "style": "margin-top:1%; min-height: 50%;" }, [
                 m("div", { "class": "card-header" }, [header]),
                 m("div", { "class": "card-block" }, [content])
