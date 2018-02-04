e2732284d6ff5f4a0ea65caca6d0d73d525691d2
pcpl2 <patryk@bimgam.es>
2018-02-04 21:23:57 +0100

    Small update.

--- js/app/videoServiceSupport/DriveGoogleCom.js
+++ js/app/videoServiceSupport/DriveGoogleCom.js
@@ -1,13 +1,14 @@
 this.DriveGoogleCom = {
     domain : "drive.google.com",
+    domains: ["drive.google.com", "docs.google.com"],
 
     register: function () {
-        return VideoServiceSupport.list.push({ api: DriveGoogleCom, id: "google", domain: this.domain });
+        return VideoServiceSupport.list.push({ api: DriveGoogleCom, id: "google", domain: this.domain, domains: this.domains });
     },
 
     getVideoUrl: function (url, returnFunction) {
         //check domain
-        if (getDomainName(url) != this.domain) {
+        if (_.find(self.domains, (stringDomain) => { return getDomainName(url) == stringDomain }) == undefined) {
             returnFunction("", VideoDecoderErrorCodes.INVALID_DOMAIN);
             return;
         }
