bceeb966335fb065a72f4f51e92f70bfaf6fc947
pcpl2 <patryykk12@gmail.com>
2018-02-10 23:19:06 +0100

    Small fix.

--- js/app/utils/utils.js
+++ js/app/utils/utils.js
@@ -1,5 +1,6 @@
 
-function DecodeCloudflareEmailProtect(e, t, r, n) {
+function DecodeCloudflareEmailProtect(e) {
+    let r, n, t = 0;
     for (r = '', n = '0x' + e.substr(t, 2) | 0, t += 2; t < e.length; t += 2)
         r += String.fromCharCode('0x' + e.substr(t, 2) ^ n);
     return r
